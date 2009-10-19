38da782f5b3920058abc273dff61196d2558b4f8
Geoffrey Wiseman <geoffrey.wiseman@gmail.com>
2009-10-19 14:20:11 -0400

    Minor enhancements to test documentation and name.

--- src/test/java/com/codiform/moo/ArrayPropertyTranslationTest.java
+++ src/test/java/com/codiform/moo/ArrayPropertyTranslationTest.java
@@ -4,7 +4,6 @@ import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertNotSame;
 
 import org.junit.Assert;
-import org.junit.Ignore;
 import org.junit.Test;
 
 /**
--- src/test/java/com/codiform/moo/SimpleUpdateTest.java
+++ src/test/java/com/codiform/moo/SimpleUpdateTest.java
@@ -6,10 +6,19 @@ import org.junit.Test;
 
 import com.codiform.moo.curry.Update;
 
+/**
+ * A test for the simple cases of update.
+ * 
+ * <p>There are lots of ugly edge cases in update; it might be best to simply suggest that you stay
+ * away from update except in simple cases, rather than attempt to handle all the ugly possibilities.<p>
+ */
 public class SimpleUpdateTest {
 
+	/**
+	 * Can update instance values on an instance; this is pure reflection, not taking Translation into account yet.
+	 */
 	@Test
-	public void testCanUpdateInstance() {
+	public void testCanUpdateInstanceValues() {
 		Ordinal ordinal = new Ordinal(1, "First");
 		Assert.assertEquals(1, ordinal.getRank());
 		Assert.assertEquals("First", ordinal.getName());
