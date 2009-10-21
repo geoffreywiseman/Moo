bf9d1307be4765171773304f89132ba0a7a5d424
Geoffrey Wiseman <geoffrey.wiseman@gmail.com>
2009-10-21 00:51:14 -0400

    Javadoc for the root translation exception.

--- src/main/java/com/codiform/moo/TranslationException.java
+++ src/main/java/com/codiform/moo/TranslationException.java
@@ -1,11 +1,28 @@
 package com.codiform.moo;
 
+/**
+ * Runtime exception that acts as the root exception for errors that might
+ * take place while working with Moo.  There are a number of possible sources
+ * for this error.  It is not currently expected that you'll try and recover
+ * from these. 
+ */
 public class TranslationException extends RuntimeException {
 	
+	/**
+	 * Create a translation exception with a message and an exception that caused the translation error.
+	 * 
+	 * @param message a message indicating what the error was
+	 * @param cause the exception that caused this one
+	 */
 	public TranslationException( String message, Throwable cause ) {
 		super( message, cause );
 	}
 
+	/**
+	 * Create a translation exception with a message.
+	 * 
+	 * @param message a message indicating what the error was
+	 */
 	public TranslationException( String message ) {
 		super( message );
 	}
