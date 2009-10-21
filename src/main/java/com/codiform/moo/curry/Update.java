37a384885ce7eb59ca0a6c99bbd150438e222373
Geoffrey Wiseman <geoffrey.wiseman@gmail.com>
2009-10-21 10:07:17 -0400

    Documented the curry.Update class.

--- src/main/java/com/codiform/moo/curry/Update.java
+++ src/main/java/com/codiform/moo/curry/Update.java
@@ -3,9 +3,23 @@ package com.codiform.moo.curry;
 import com.codiform.moo.configuration.Configuration;
 import com.codiform.moo.session.TranslationSession;
 
+/**
+ * A curried update operation, where the source of the update has been
+ * specified, but the object to which updates should be applied has not.
+ * 
+ * <p>This can be used to perform updates on several objects, or simply to
+ * act as a kind of easy-to-use DSL.</p>
+ */
 public class Update {
 
+	/**
+	 * The source of data to be applied as updates.
+	 */
 	private Object source;
+	
+	/**
+	 * The configuration backing the updates.
+	 */
 	private Configuration configuration;
 	
 	public Update( Configuration configuration, Object source ) {
@@ -13,10 +27,21 @@ public class Update {
 		this.source = source;
 	}
 	
+	/**
+	 * Apply the update from the source to the specified destination.
+	 * 
+	 * @param destination the object to which updates should be applied.
+	 */
 	public void to( Object destination ) {
 		new TranslationSession( configuration ).update(source, destination);
 	}
 	
+	/**
+	 * Create a curried update based on a particular source.
+	 * 
+	 * @param source the source of the update
+	 * @return the curried update operation
+	 */
 	public static Update from( Object source ) {
 		return new Update( new Configuration(), source );
 	}
