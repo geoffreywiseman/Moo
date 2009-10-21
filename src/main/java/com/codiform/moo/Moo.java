8de3e51bb023dcab8b44a071f15d5f72e089f51c
Geoffrey Wiseman <geoffrey.wiseman@gmail.com>
2009-10-21 00:40:14 -0400

    Added Javadoc to the primary facade class.

--- src/main/java/com/codiform/moo/Moo.java
+++ src/main/java/com/codiform/moo/Moo.java
@@ -28,41 +28,94 @@ import com.codiform.moo.session.TranslationSession;
  * Moo supports a limited amount of configuration through annotations, in 
  * particular {@link Translation}, {@link Translate} and {@link TranslateCollection}.
  * </p>
+ * 
+ * <p>
+ * Instances of Moo are intended to be typesafe, they can be used by more than
+ * one thread at once without an issue, and can be cached and re-used.
+ * </p>
  */
 public class Moo {
 
 	private Configuration configuration;
 
+	/**
+	 * Creates a new instance of Moo with a default {@link Configuration}.
+	 */
 	public Moo( ) {
 		this( new Configuration() );
 	}
 	
+	/**
+	 * Creates a new instance of Moo with a known (and specified) {@link Configuration}.
+	 * 
+	 * @param configuration the configuration for this instance of Moo
+	 */
 	public Moo( Configuration configuration ) {
 		this.configuration = configuration;
+	}
+
+	/**
+	 * Prepare for translation to a destination class by creating a 'curried' operation
+	 * which can be invoked upon to do translations.
+	 * 
+	 * @param <T> the type to which translations should be performed
+	 * @param destinationClass the class for the type to which translations should be performed
+	 * @return a class representing the 'curried' {@link Translate}
+	 */
+	public <T> Translate<T> translate( Class<T> destinationClass ) {
+		return new Translate<T>( configuration, destinationClass );
+	}
+
+	/**
+	 * Perform a translation of a source object into an instance of the destination class.
+	 * 
+	 * @param <T> the type to which translation should take place
+	 * @param source the object to be used as the source of the translation
+	 * @param destinationClass the class of the type to which translation should take place
+	 * @return the instance of the destination class with values translated from the source instance
+	 */
+	public <T> T translate( Object source, Class<T> destinationClass ) {
+		return newSession().getTranslation( source, destinationClass );
+	}
+
+	/**
+	 * Perform a translation of a list of source objects into an list of instances of the destination class.
+	 * 
+	 * @param <T> the type to which translation should take place
+	 * @param sources the objects to be used as the sources of the translation
+	 * @param destinationClass the class of the type to which translation should take place
+	 * @return a list of instances of the destination class with values translated from the source instances
+	 */
+	public <T> List<T> translateEach( List<?> sources, Class<T> destinationClass ) {
+		return newSession().getEachTranslation( sources, destinationClass );
+	}
+
+	/**
+	 * Perform a translation of a collection of source objects into an collection of instances of the destination class.
+	 * 
+	 * @param <T> the type to which translation should take place
+	 * @param sources the objects to be used as the sources of the translation
+	 * @param destinationClass the class of the type to which translation should take place
+	 * @return a collection of instances of the destination class with values translated from the source instances
+	 */
+	public <T> Collection<T> translateEach( Collection<?> sources, Class<T> destinationClass ) {
+		return newSession().getEachTranslation( sources, destinationClass );
+	}
+
+	/**
+	 * Perform a translation of a set of source objects into an set of instances of the destination class.
+	 * 
+	 * @param <T> the type to which translation should take place
+	 * @param sources the objects to be used as the sources of the translation
+	 * @param destinationClass the class of the type to which translation should take place
+	 * @return a set of instances of the destination class with values translated from the source instances
+	 */
+	public <T> Set<T> translateEach( Set<?> sources, Class<T> destinationClass ) {
+		return newSession().getEachTranslation( sources, destinationClass );
 	}
 
 	private TranslationSession newSession() {
 		return new TranslationSession(configuration);
 	}
 	
-	public <T> Translate<T> translate( Class<T> destinationClass ) {
-		return new Translate<T>( configuration, destinationClass );
-	}
-
-	public <T> T translate( Object source, Class<T> destinationClass ) {
-		return newSession().getTranslation( source, destinationClass );
-	}
-
-	public <T> List<T> translateEach( List<?> sources, Class<T> destinationClass ) {
-		return newSession().getEachTranslation( sources, destinationClass );
-	}
-
-	public <T> Collection<T> translateEach( Collection<?> sources, Class<T> destinationClass ) {
-		return newSession().getEachTranslation( sources, destinationClass );
-	}
-
-	public <T> Set<T> translateEach( Set<?> sources, Class<T> destinationClass ) {
-		return newSession().getEachTranslation( sources, destinationClass );
-	}
-
 }
