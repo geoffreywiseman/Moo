a5dcfd9ad2f562dbca6fc8027a49896be183baf3
Geoffrey Wiseman <geoffrey.wiseman@gmail.com>
2011-12-22 16:46:02 -0500

    This should have been added before as part of #68.

---
+++ src/test/java/com/codiform/moo/TranslationConstructionTest.java
@@ -0,0 +1,61 @@
+package com.codiform.moo;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import com.codiform.moo.curry.Translate;
+
+public class TranslationConstructionTest {
+
+	@Test
+	public void testCanTranslateToClassesWithProtectedNoArgConstructor() {
+		Source source = new Source( "Translate to Class with Protected Constructor" );
+		ProtectedDestination destination = Translate.to( ProtectedDestination.class ).from( source );
+		assertEquals( "Translate to Class with Protected Constructor", destination.getName() );
+	}
+
+	@Test
+	public void testCanTranslateToClassesWithPrivateNoArgConstructor() {
+		Source source = new Source( "Translate to Class with Private Constructor" );
+		PrivateDestination destination = Translate.to( PrivateDestination.class ).from( source );
+		assertEquals( "Translate to Class with Private Constructor", destination.getName() );
+	}
+	
+	public static class Source {
+		private String name;
+		
+		public Source( String name ) {
+			this.name = name;
+		}
+		
+		public String getName() {
+			return name;
+		}
+	}
+	
+	public static class ProtectedDestination {
+		private String name;
+		
+		protected ProtectedDestination() {
+			// for Moo
+		}
+		
+		public String getName() {
+			return name;
+		}
+	}
+
+	public static class PrivateDestination {
+		private String name;
+		
+		private PrivateDestination() {
+			// for Moo
+		}
+		
+		public String getName() {
+			return name;
+		}
+	}
+
+}
