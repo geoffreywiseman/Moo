684d43d3cddc740afa0f0b23f14d175ca3a4edd7
Geoffrey Wiseman <geoffrey.wiseman@gmail.com>
2011-12-23 10:47:00 -0500

    Additional tests for lookup on translation and update. Closes #49.

--- src/test/java/com/codiform/moo/TranslationLookupTest.java
+++ src/test/java/com/codiform/moo/TranslationLookupTest.java
@@ -6,11 +6,11 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 
 import com.codiform.moo.annotation.Property;
 import com.codiform.moo.curry.Translate;
+import com.codiform.moo.curry.Update;
 
 /**
  * A test showing how a translation/update might invoke a lookup based on
@@ -22,7 +22,8 @@ public class TranslationLookupTest {
 
 	@Before
 	public void setUp() {
-		userDao = new UserDao( new User( 38, "Penelope Williams-Ward", "pwilliams", 7 ) );
+		userDao = new UserDao( new User( 38, "Penelope Williams-Ward",
+				"pwilliams", 7 ) );
 	}
 
 	@Test
@@ -39,21 +40,33 @@ public class TranslationLookupTest {
 	}
 
 	@Test
-	@Ignore("TBI")
 	public void testCanLookupPropertyOnUpdate() {
-
+		PostDto postDto = new PostDto(
+				"Saving Money through Extreme Couponing", new AuthorDto( 38,
+						"Penelope Williams", 6 ) );
+		Post post = new Post( "TBD ABout Saving Money", userDao.getUser( 38 ) );
+		
+		Update.from( postDto ).withVariable( "userDao", userDao ).to( post );
+		
+		assertEquals( postDto.getTitle(), post.getTitle() );
+		assertEquals( Integer.valueOf( 38 ), post.getAuthor().getId() );
+		assertEquals( 7, post.getAuthor().getVersion() );
+		assertEquals( "pwilliams", post.getAuthor().getUsername() );
 	}
 
-	@Test
-	@Ignore("TBI")
+	@Test(expected=TranslationException.class)
 	public void testCanThrowExceptionOnLookupFailure() {
-
+		PostDto postDto = new PostDto(
+				"Love on the High Seas", new AuthorDto( 45,
+						"Saul Madison", 1 ) );
+		Translate.to( Post.class ).withVariable(
+				"userDao", userDao ).from( postDto );
 	}
 
 	public static class PostDto {
 		private String title;
 		private AuthorDto author;
-		
+
 		public PostDto(String title, AuthorDto author) {
 			this.title = title;
 			this.author = author;
@@ -62,12 +75,13 @@ public class TranslationLookupTest {
 		public String getTitle() {
 			return title;
 		}
-		
+
 		public AuthorDto getAuthor() {
 			return author;
 		}
 	}
 
+	@SuppressWarnings("unused")
 	public static class AuthorDto {
 		public AuthorDto(Integer id, String displayName, int version) {
 			this.id = id;
@@ -78,17 +92,27 @@ public class TranslationLookupTest {
 		private Integer id;
 		private String displayName;
 		private int version;
-		
+
 		public Integer getId() {
 			return id;
 		}
 	}
 
+	@SuppressWarnings("unused")
 	public static class Post {
 		private String title;
 
-		@Property(translation = "userDao.getUser(38)")
+		@Property(translation = "userDao.getUser(author.id)")
 		private User author;
+
+		private Post( ) {
+			// no-arg constructor for translation
+		}
+		
+		public Post(String title, User author) {
+			this.title = title;
+			this.author = author;
+		}
 
 		public String getTitle() {
 			return title;
@@ -99,6 +123,7 @@ public class TranslationLookupTest {
 		}
 	}
 
+	@SuppressWarnings("unused")
 	public static class User {
 		private Integer id;
 		private String displayName;
@@ -140,7 +165,7 @@ public class TranslationLookupTest {
 				return usersByKey.get( id );
 			} else {
 				throw new IllegalArgumentException(
-						"Cannot find user correspond to key " + id );
+						"Cannot find user corresponding to key " + id );
 			}
 		}
 	}
