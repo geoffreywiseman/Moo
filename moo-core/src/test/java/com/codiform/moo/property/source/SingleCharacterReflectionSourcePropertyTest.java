a5aec201bbd15c92fbf3472a8de8b7f84abcaf99
Geoffrey Wiseman <geoffrey.wiseman@gmail.com>
2013-05-27 16:04:54 -0400

    Tests for Single-Character Properties
    Added two tests for the ReflectionSourceProperty to ensure that it works with single-character
    fields and methods, since these were an edge case that I'd written down during the implementation
    as a potential concern.
    
    They worked, so this is additional test code with no changes to the reflection source property.
    
    Related to #14.

---
+++ moo-core/src/test/java/com/codiform/moo/property/source/SingleCharacterReflectionSourcePropertyTest.java
@@ -0,0 +1,41 @@
+package com.codiform.moo.property.source;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class SingleCharacterReflectionSourcePropertyTest {
+
+	ReflectionSourceProperty rsp = new ReflectionSourceProperty( "v" );
+
+	@Test
+	public void testReflectionSourcePropertyCanRetrieveValueFromField() {
+		PublicFieldSource source = new PublicFieldSource();
+		source.v = "Public Field Value";
+		assertEquals( "Public Field Value", rsp.getValue( source ) );
+	}
+
+	@Test
+	public void testReflectionSourcePropertyCanRetrieveValueFromGetter() {
+		PublicGetGetterSource source = new PublicGetGetterSource( 242 );
+		assertEquals( 242, rsp.getValue( source ) );
+	}
+
+	private static class PublicFieldSource {
+		@SuppressWarnings( "unused" )
+		public String v;
+	}
+
+	private static class PublicGetGetterSource {
+		public PublicGetGetterSource( int value ) {
+			this.hidden = value;
+		}
+
+		private int hidden;
+
+		@SuppressWarnings( "unused" )
+		public int getV() {
+			return hidden;
+		}
+	}
+}
