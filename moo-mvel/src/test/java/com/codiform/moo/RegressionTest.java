f663746848cbde56291aec630bc148deb84b3371
Geoffrey Wiseman <geoffrey.wiseman@gmail.com>
2013-05-27 17:47:44 -0400

    Regression Test for Private Field Access
    One of the triggers for separating MVEL from moo-core was that MVEL made it hard to
    access private properties. This test ensures that it is still possible to access
    private properties once moo-mvel is in place.
    
    Related to #14.

---
+++ moo-mvel/src/test/java/com/codiform/moo/RegressionTest.java
@@ -0,0 +1,42 @@
+package com.codiform.moo;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import com.codiform.moo.curry.Translate;
+
+/**
+ * Check that the presence of moo-mvel doesn't cause regressions for Moo core.
+ */
+public class RegressionTest {
+
+	/**
+	 * Although the separation of moo-mvel from moo-core had been planned, the trigger for making 
+	 * it move from planned to in-progress was support for having private source property fields.
+	 * 
+	 * The presence of moo-mvel should still make it possible to access source property fields.
+	 */
+	@Test
+	public void testPrivateFieldSourcePropertyViaReflection() {
+		PrivateFieldSource source = new PrivateFieldSource( "No regression here." );
+		Destination destination = Translate.to( Destination.class ).from( source );
+		assertEquals( "No regression here.", destination.getValue() );
+	}
+	
+	
+	private static class Destination {
+		private String value;
+		public String getValue() {
+			return value;
+		}
+	}
+	
+	private static class PrivateFieldSource {
+		@SuppressWarnings( "unused" )
+		private String value;
+		public PrivateFieldSource( String value ) {
+			this.value = value;
+		}
+	}
+}
