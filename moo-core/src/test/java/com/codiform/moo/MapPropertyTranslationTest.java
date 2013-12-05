009dd8b8a6571c0eaa7f74a5a5d046ebb3212e6b
Geoffrey Wiseman <geoffrey.wiseman@gmail.com>
2013-12-04 22:23:07 -0500

    Test for No-Defensive-Copy of Maps
    Test that maps will be used as is if there's no defensive copying of the map.

--- moo-core/src/test/java/com/codiform/moo/MapPropertyTranslationTest.java
+++ moo-core/src/test/java/com/codiform/moo/MapPropertyTranslationTest.java
@@ -2,28 +2,47 @@ package com.codiform.moo;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotSame;
+import static org.junit.Assert.assertSame;
 
 import java.util.HashMap;
 import java.util.Map;
 
 import org.junit.Test;
 
+import com.codiform.moo.configuration.Configuration;
 import com.codiform.moo.curry.Translate;
 
 public class MapPropertyTranslationTest {
 
 	@Test
-	public void testTranslateCopiesMap() {
+	public void testTranslateWithoutAnnotationsCopiesMap() {
 		StockPrices domain = new StockPrices();
 		domain.setPrice( "AAPL", 246.25 );
 		domain.setPrice( "MSFT", 28.98 );
 		domain.setPrice( "ORCL", 23.91 );
 
-		StockPricesDto dto = Translate.to( StockPricesDto.class ).from( domain );
+		StockPricesCopy dto = Translate.to( StockPricesCopy.class ).from( domain );
 
 		assertNotSame( domain.getPrices(), dto.getPrices() );
 		assertEquals( domain.getPrices(), dto.getPrices() );
 	}
+
+	@Test
+	public void testTranslateWithoutDefensiveCopyUsesSameMap() {
+		StockPrices domain = new StockPrices();
+		domain.setPrice( "JAVA", 112.04 );
+		domain.setPrice( "NODE", 35.12 );
+		domain.setPrice( "RUBY", 38.53 );
+
+		Configuration configuration = new Configuration();
+		configuration.setPerformingDefensiveCopies( false );
+		
+		StockPricesCopy dto = new Moo( configuration ).translate( domain, StockPricesCopy.class );
+
+		assertEquals( domain.getPrices(), dto.getPrices() );
+		assertSame( domain.getPrices(), dto.getPrices() );
+	}
+
 
 	public static class StockPrices {
 		private Map<String, Double> prices = new HashMap<String, Double>();
@@ -38,7 +57,7 @@ public class MapPropertyTranslationTest {
 
 	}
 
-	public static class StockPricesDto {
+	public static class StockPricesCopy {
 		private Map<String, Double> prices = new HashMap<String, Double>();
 
 		public Map<String, Double> getPrices() {
